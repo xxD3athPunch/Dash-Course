6116024dbb2c513ffa7d848698850057cb710d4a
xxD3athPunch <68506158+xxD3athPunch@users.noreply.github.com>
2020-07-29 12:40:22 -0700

    .

--- LineCharts.Atom.py
+++ LineCharts.Atom.py
@@ -9,4 +9,9 @@ df2.set_index('NAME', inplace=True)
 list_of_pop_col = [col for col in df2.columns if col.startswith('POP')]
 df2 = df2[list_of_pop_col]
 
-print(df2)
+data = [ go.Scatter(x=df2.columns,
+                    y=df2.loc[name],
+                    mode='lines',
+                    name=name) for name in df2.index]
+
+pyo.plot(data)
